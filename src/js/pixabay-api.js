d4db6c15ea6b16d45b1ea402c7f069c3ce5cdd07
Mahuliii <148713111+Mahuliii@users.noreply.github.com>
2024-04-01 00:56:27 +0300

    add2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,8 +1,9 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+
 const API_KEY = '43146074-d3428091907388d5fa71ff65d';
 
-export async function fetchImages(searchTerm) {
+export function fetchImages(searchTerm) {
   const queryParams = new URLSearchParams({
     key: API_KEY,
     q: searchTerm,
@@ -11,25 +12,26 @@ export async function fetchImages(searchTerm) {
     safesearch: true,
   });
 
-  try {
-    const response = await fetch(
-      `https://pixabay.com/api/?${queryParams.toString()}`
-    );
-    if (!response.ok) {
+  return fetch(`https://pixabay.com/api/?${queryParams.toString()}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch images');
+      }
+      return response.json();
+    })
+    .then(data => {
+      if (data.hits.length === 0) {
+        iziToast.show({
+          backgroundColor: 'rgba(239, 64, 64, 1)',
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          messageColor: 'rgba(250, 250, 251, 1)',
+          position: 'center',
+        });
+      }
+      return data.hits;
+    })
+    .catch(error => {
       throw new Error('Failed to fetch images');
-    }
-    const data = await response.json();
-    if (data.hits.length === 0) {
-      iziToast.show({
-        backgroundColor: 'rgba(239, 64, 64, 1)',
-        message:
-          'Sorry, there are no images matching your search query. Please try again!',
-        messageColor: 'rgba(250, 250, 251, 1)',
-        position: 'center',
-      });
-    }
-    return data.hits;
-  } catch (error) {
-    throw new Error('Failed to fetch images');
-  }
+    });
 }
